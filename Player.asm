39ce9e8f49f50aca8f8f0a4d16bb7ea3bbb75aab
hazemtarekelaswad <hazem.elaswad00@eng-st.cu.edu.eg>
2021-01-01 01:19:42 +0200

    Fix coin.asm bug and add it to Player.asm

--- Player.asm
+++ Player.asm
@@ -508,10 +508,38 @@ LOCAL DRAW, FILL_HEIGHT, FILL_LEN
     JNE DRAW
 ENDM
 
+; X of coins 5, 15, 25, 35, 45, 55, 65, 75
+; Y of coins 1, 4, 6, 9, 12, 14, 17, 20
+; 10h, 4F02H ==> 79 X 24
+DRAW_COIN MACRO X, Y
+    XOR BX, BX
+    int 10h
+
+; Move the cursor to x, y positions
+    mov cx, 1        ; number of drawings
+    mov dl, X        ; x coordinate
+    mov dh, Y        ; y coordinate
+    mov ah, 2
+    int 10h
+
+; Display character in video mode
+    mov al,'O'
+    mov ah, 09h
+    mov bl, 0Eh      ; Color
+    int 10h
+endm 
+
+CLEAR_SCREEN PROC
+    MOV AX, 0013H
+    INT 10H
+    RET
+CLEAR_SCREEN ENDP
+
 CHANGE_TO_VIDEO PROC 
+	; Change to video mode 640 X 400 X 256
 	MOV AX, 4F02H
-	MOV BX, 0100H    
-	INT 10H         
+    MOV BX, 0100H
+    int 10h        
 	; MOV AH,0Bh 
 	RET
 ENDP
@@ -526,14 +554,13 @@ MAIN PROC FAR
 	DRAW_PLAYER PLAYER_IMG2, X_POS2, Y_POS2
 
 	DRAW_FILLED_CUBOID 30, 15, 15, 12, 15
-	DRAW_FILLED_CUBOID 110, 58, 15, 12, 15
-	DRAW_FILLED_CUBOID 410, 101, 15, 12, 15
-	DRAW_FILLED_CUBOID 100, 101, 15, 12, 15
-	DRAW_FILLED_CUBOID 39, 150, 15, 12, 15
-	DRAW_FILLED_CUBOID 88, 301, 15, 12, 15
-	DRAW_FILLED_CUBOID 294, 198, 15, 12, 15
-	DRAW_FILLED_CUBOID 200, 280, 15, 12, 15
-
+	DRAW_FILLED_CUBOID 110, 144, 15, 12, 15
+	DRAW_FILLED_CUBOID 350, 58, 15, 12, 15
+	DRAW_FILLED_CUBOID 510, 187, 15, 12, 15
+	DRAW_FILLED_CUBOID 350, 236, 15, 12, 15
+	DRAW_FILLED_CUBOID 590, 322, 15, 12, 15
+	DRAW_COIN 5, 20
+	
 ; Infinite loop that lets the user move players all around the grid
 	INFINITE:
 	; Check if the a key is pressed

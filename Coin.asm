29078b67b8d839cf1de903ee45b20c564fed61f9
hazemtarekelaswad <hazem.elaswad00@eng-st.cu.edu.eg>
2020-12-31 13:44:03 +0200

    Change to upper case in coin.asm

--- Coin.asm
+++ Coin.asm
@@ -1,22 +1,27 @@
-.model small
-.code
-coin macro  x,y
-    
- 
-mov ah,0
-mov al,13h
-mov bh,0
-int 10h
+.MODEL SMALL
+.STACK 64
+.CODE
+COIN MACRO X, Y
+    MOV AX, 4F02H
+    MOV BX, 0100H
+    int 10h
 
-mov cx,1 ; number of drawings
-mov dl,x ; x coordinate
-mov dh,y; y coordinate
+; Move the cursor to x, y positions
+    mov cx, 1        ; number of drawings
+    mov dl, X        ; x coordinate
+    mov dh, Y        ; y coordinate
 
-mov ah,2
-int 10h
+    mov ah, 2
+    int 10h
 
-mov al,'O'
-mov ah,9h
-mov bl,0Eh
-int 10h
- endm 
+; Display character in video mode
+    mov al,'O'
+    mov ah, 9h
+    mov bl, 0Eh      ; Color
+    int 10h
+endm 
+
+MAIN PROC FAR
+    COIN 40, 20
+MAIN ENDP
+END MAIN
